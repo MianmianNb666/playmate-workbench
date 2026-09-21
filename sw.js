6b4c3edeca63d3af36f99981b13343119928e1b9
MianmianNb666 <jiaj200405@gmail.com>
2026-09-21 18:04:27 +1000

    Add Pai mini service worker

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME="paimini-v1";
+const APP_SHELL=["./","./index.html","./styles.css","./guide.html","./manifest.webmanifest","./icon.svg"];
+
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).catch(()=>{})
+  );
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(
+    fetch(event.request).catch(()=>caches.match(event.request))
+  );
+});
